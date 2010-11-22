ff90e97b14dfa39268c6647c2ac58cf46c78c657
ambrop7 <ambrop7@1a93d707-3861-5ebc-ad3b-9740d49b5140>
2010-11-22 22:01:12 +0000

    version 1.999.96rc17

--- misc/version.h
+++ misc/version.h
@@ -28,7 +28,7 @@
 #define BADVPN_MISC_VERSION_H
 
 #define GLOBAL_PRODUCT_NAME "BadVPN"
-#define GLOBAL_VERSION "1.999.96rc16"
+#define GLOBAL_VERSION "1.999.96rc17"
 #define GLOBAL_COPYRIGHT_NOTICE "Copyright (C) 2010 Ambroz Bizjak <ambrop7@gmail.com>"
 
 #endif
