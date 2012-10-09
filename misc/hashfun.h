aafa614ea8af1d181eca2ec47d6e256500450106
ambrop7 <ambrop7@1a93d707-3861-5ebc-ad3b-9740d49b5140>
2012-10-09 19:47:55 +0000

    misc/hashfun.h: add badvpn_djb2_hash_bin()

--- misc/hashfun.h
+++ misc/hashfun.h
@@ -45,4 +45,16 @@ static size_t badvpn_djb2_hash (const uint8_t *str)
     return hash;
 }
 
+static size_t badvpn_djb2_hash_bin (const uint8_t *str, size_t str_len)
+{
+    size_t hash = 5381;
+    
+    while (str_len-- > 0) {
+        int c = *str++;
+        hash = ((hash << 5) + hash) + c;
+    }
+    
+    return hash;
+}
+
 #endif
