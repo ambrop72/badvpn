fe474df5317e03ecc50e636ab4629bfcc9398f95
ambrop7 <ambrop7@1a93d707-3861-5ebc-ad3b-9740d49b5140>
2012-10-19 11:55:32 +0000

    ncd: add boolean.h

---
+++ ncd/boolean.h
@@ -0,0 +1,57 @@
+/**
+ * @file boolean.h
+ * @author Ambroz Bizjak <ambrop7@gmail.com>
+ * 
+ * @section LICENSE
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. Neither the name of the author nor the
+ *    names of its contributors may be used to endorse or promote products
+ *    derived from this software without specific prior written permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#ifndef NCD_BOOLEAN_H
+#define NCD_BOOLEAN_H
+
+#include <misc/debug.h>
+#include <ncd/NCDVal.h>
+#include <ncd/static_strings.h>
+
+static NCDValRef ncd_make_boolean (NCDValMem *mem, int value, NCDStringIndex *string_index)
+{
+    ASSERT(mem)
+    ASSERT(string_index)
+    
+    NCD_string_id_t str_id = (value ? NCD_STRING_TRUE : NCD_STRING_FALSE);
+    return NCDVal_NewIdString(mem, str_id, string_index);
+}
+
+static int ncd_read_boolean (NCDValRef val)
+{
+    ASSERT(NCDVal_IsString(val))
+    
+    if (NCDVal_IsIdString(val)) {
+        return NCDVal_IdStringId(val) == NCD_STRING_TRUE;
+    } else {
+        return NCDVal_StringEquals(val, "true");
+    }
+}
+
+#endif
