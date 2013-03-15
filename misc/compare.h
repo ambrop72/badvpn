a2044b6ce7185848f1c7bc381a06005ef7763305
ambrop7 <ambrop7@1a93d707-3861-5ebc-ad3b-9740d49b5140>
2013-03-15 02:40:25 +0000

    misc/compare.h: add B_COMPARE2() macro

--- misc/compare.h
+++ misc/compare.h
@@ -31,5 +31,7 @@
 #define BADVPN_COMPARE_H
 
 #define B_COMPARE(a, b) (((a) > (b)) - ((a) < (b)))
+#define B_COMPARE_COMBINE(cmp1, cmp2) ((cmp1) ? (cmp1) : (cmp2))
+#define B_COMPARE2(a, b, c, d) B_COMPARE_COMBINE(B_COMPARE((a), (b)), B_COMPARE((c), (d)))
 
 #endif
