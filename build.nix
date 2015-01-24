1be45b445dad198e7ca0314ec2cd6259318d33f3
Ambroz Bizjak <ambrop7@gmail.com>
2015-01-24 15:36:38 +0100

    Nix expressions: Separate the package function from the composition.

--- build.nix
+++ build.nix
@@ -1,22 +1,6 @@
-let
-    badvpnLocal = (
-        { stdenv, cmake, pkgconfig, openssl, nspr, nss, debug ? false }:
-        let
-            compileFlags = "-O3 ${stdenv.lib.optionalString (!debug) "-DNDEBUG"}";
-        in
-        stdenv.mkDerivation {
-            name = "badvpn";
-            nativeBuildInputs = [ cmake pkgconfig ];
-            buildInputs = [ openssl nspr nss ];
-            src = stdenv.lib.cleanSource ./.;
-            preConfigure = ''
-                cmakeFlagsArray=( "-DCMAKE_BUILD_TYPE=" "-DCMAKE_C_FLAGS=${compileFlags}" );
-            '';
-        }
-    );
-in
 with import <nixpkgs> {};
 rec {
-   badvpn = pkgs.callPackage badvpnLocal {};
-   badvpnDebug = pkgs.callPackage badvpnLocal { debug = true; };
+    badvpnFunc = import ./badvpn.nix;
+    badvpn = pkgs.callPackage badvpnFunc {};
+    badvpnDebug = pkgs.callPackage badvpnFunc { debug = true; };
 }
