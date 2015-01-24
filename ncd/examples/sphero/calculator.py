d905e48bb5b47ba49d407fdddc21e0e0758bf559
Ambroz Bizjak <ambrop7@gmail.com>
2015-01-24 12:02:59 +0100

    spheroncd: Throttle for speed scaling, refactoring.

--- ncd/examples/sphero/calculator.py
+++ ncd/examples/sphero/calculator.py
@@ -1,25 +1,26 @@
-from __future__ import print_function
 import sys
-import argparse
 import math
 
+def compute_rel_signed(val, minval, maxval):
+    return max(-1.0, min(1.0, (2.0 * ((val - minval) / (maxval - minval)) - 1.0)))
+
+def compute_rel_unsigned(val, minval, maxval):
+    return max(0.0, min(1.0, ((val - minval) / (maxval - minval))))
+
 def main():
-    parser = argparse.ArgumentParser()
-    args = parser.parse_args()
-    
     while True:
         comps = sys.stdin.readline().rstrip('\n').split(' ')
-        assert len(comps) == 11
         numbers = [float(x) for x in comps]
         
-        x_val, x_min, x_max, y_val, y_min, y_max, rz_val, rz_min, rz_max, turbo, adjust = numbers
+        x_val, x_min, x_max, y_val, y_min, y_max, rz_val, rz_min, rz_max, rz_degrees, throttle_pos, throttle_min, throttle_max, turbo, adjust = numbers
         
-        x_rel = 2.0 * ((x_val - x_min) / (x_max - x_min)) - 1.0
-        y_rel = 2.0 * ((y_val - y_min) / (y_max - y_min)) - 1.0
-        rz_rel = 2.0 * ((rz_val - rz_min) / (rz_max - rz_min)) - 1.0
+        x_rel  = compute_rel_signed(x_val, x_min, x_max)
+        y_rel  = compute_rel_signed(y_val, y_min, y_max)
+        rz_rel = compute_rel_signed(rz_val, rz_min, rz_max)
+        throttle_rel = compute_rel_unsigned(throttle_pos, throttle_min, throttle_max)
         
-        angle = math.atan2(y_rel, x_rel)
-        length = math.sqrt(x_rel**2 + y_rel**2)
+        angle = math.atan2(y_rel, x_rel) + (rz_rel * math.radians(rz_degrees))
+        length = math.sqrt(x_rel**2 + y_rel**2) * throttle_rel
         
         angle_fixed = (int(round(math.degrees(angle))) + 90) % 360
         speed_fixed = 0 if adjust else 255 if turbo else max(0, min(255, int(round(255.0 * length))))
