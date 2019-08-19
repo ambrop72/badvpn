d4539b496447c8510663494a44c3a3ade9237edd
Ben Schwartz <bemasc@google.com>
2019-08-19 12:47:18 -0400

    Add copyright notices for this change

--- socks_udp_client/SocksUdpClient.c
+++ socks_udp_client/SocksUdpClient.c
@@ -1,4 +1,6 @@
 /*
+ * Copyright (C) 2018 Jigsaw Operations LLC
+ * 
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
  * 1. Redistributions of source code must retain the above copyright
